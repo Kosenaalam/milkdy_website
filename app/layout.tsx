149f64bc1f7c2ab186803fe56bbb9352d921900d
Kosenaalam <kosenaalam@gmail.com>
2026-04-18 12:46:00 +0530

    fixed font issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter } from "next/font/google";
+//import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
+//const _geist = Geist({ subsets: ["latin"] });
+//const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'Milkdy - Modern Dairy Management Platform',
@@ -36,7 +38,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-sans antialiased">
+      <body className={inter.className}>
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
